af0aca216353061835d89f15dcb6b2b626fdfd77
AdrianIon218 <61580160+AdrianIon218@users.noreply.github.com>
2022-10-02 20:14:29 +0300

    Add files via upload

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -1,3 +1,4 @@
+// Model section
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const title = document.querySelector('.instruction')
 const possibleChoices = document.querySelectorAll('.card')
@@ -7,19 +8,61 @@ let userChoice
 let computerChoice
 let iscardchosen = false
 
-const winMessage = 'You won ! '
-const loseMessage = 'You lose ! '
-const drawMessage = 'It\'s a draw ! '
-
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=>{
-  if(!iscardchosen){
-    userChoice = e.target.id
-    generateComputerChoice()
-    getResult()
-    setFocus(possibleChoice)
-    iscardchosen = true
+function generateComputerChoice(){
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+  if(randomNumber == 1){
+    computerChoice = 'rock'
+    computerChoiceDisplay.setAttribute('src','images/rock.jpeg')
   }
-}))
+  if(randomNumber == 2){
+    computerChoice = 'scissors'
+    computerChoiceDisplay.setAttribute('src','images/scissors.png')
+  }
+  if(randomNumber == 3){
+    computerChoice = 'paper'
+    computerChoiceDisplay.setAttribute('src','images/paper.jpeg')
+  }
+  computerChoiceDisplay.style.visibility = "visible"
+}
+
+// View section
+function setTitle(result){
+  switch(result){
+    case 'win':
+      panel.style.setProperty('border-color','lightgreen')
+      title.classList.add('win-game')
+      title.textContent = 'You won ! '
+      title.innerHTML +='<span class="emoji">&#128522;</span>'
+      break;
+    case 'lose':
+      panel.style.setProperty('border-color','red')
+      title.classList.add('lose-game')
+      title.innerHTML = 'You lose ! '
+      title.innerHTML +='<span class="emoji">&#128580;</span>'
+      break;
+    case 'draw':
+      panel.style.setProperty('border-color','darkblue')
+      title.classList.add('draw-game')
+      title.innerHTML = 'It\'s a draw ! ' 
+      title.innerHTML +='<span class="emoji">&#128521;</span>'
+      break
+  }
+}
+
+function resetGame(){
+  panel.style.setProperty('border-color','gray')
+  iscardchosen = false;
+  possibleChoices.forEach(possibleChoice => {
+    possibleChoice.classList.remove('card_chosen')
+    possibleChoice.classList.remove('card_block')
+  })
+  title.classList.remove('win-game')
+  title.classList.remove('lose-game')
+  title.classList.remove('draw-game')
+  title.textContent = 'Choose a card '
+  title.innerHTML += '<span class="emoji">&#129488;</span>'
+  computerChoiceDisplay.style.visibility = "hidden"
+}
 
 function setFocus(cardChosen){
   possibleChoices.forEach(possibleChoice => {
@@ -32,79 +75,37 @@ function setFocus(cardChosen){
   cardChosen.classList.add('card_chosen')
 }
 
-function generateComputerChoice(){
-  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
-  if(randomNumber == 1){
-    computerChoice = "rock"
-    computerChoiceDisplay.setAttribute('src','images/rock.jpeg')
+// Controllers section
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=>{
+  if(!iscardchosen){
+    userChoice = e.target.id
+    generateComputerChoice()
+    getResult()
+    setFocus(possibleChoice)
+    iscardchosen = true
   }
-  if(randomNumber == 2){
-    computerChoice = 'scissors'
-    computerChoiceDisplay.setAttribute('src','images/scissors.png')
-  }
-  if(randomNumber == 3){
-    computerChoice = 'paper'
-    computerChoiceDisplay.setAttribute('src','images/paper.jpeg')
-  }
-  computerChoiceDisplay.style.visibility = "visible"
-  computerChoiceDisplay.innerHTML = computerChoice
-}
+}))
 
 function getResult(){
-  if(computerChoice == userChoice){
-    title.innerHTML = drawMessage 
-    title.innerHTML +='<span class="emoji">&#128521;</span>'
+  if(computerChoice === userChoice){
+    setTitle('draw')
   }
-  if(computerChoice == 'rock' && userChoice=='paper'){
-    panel.style.setProperty('border-color','lightgreen')
-    title.classList.add('win-game')
-    title.textContent = winMessage
-    title.innerHTML +='<span class="emoji">&#128522;</span>'
-  } 
-  if(computerChoice == 'rock' && userChoice=='scissors'){
-    panel.style.setProperty('border-color','red')
-    title.classList.add('lose-game')
-    title.innerHTML = loseMessage
-    title.innerHTML +='<span class="emoji">&#128580;</span>'
-  } 
-  if(computerChoice == 'paper' && userChoice=='scissors'){
-    panel.style.setProperty('border-color','lightgreen')
-    title.classList.add('win-game')
-    title.innerHTML = winMessage
-    title.innerHTML +='<span class="emoji">&#128522;</span>'
-  } 
-  if(computerChoice == 'paper' && userChoice=='rock'){
-    panel.style.setProperty('border-color','red')
-    title.classList.add('lose-game')
-    title.innerHTML = loseMessage
-    title.innerHTML +='<span class="emoji">&#128580;</span>'
-  } 
-  if(computerChoice == 'scissors' && userChoice=='paper'){
-    panel.style.setProperty('border-color','red')
-    title.classList.add('lose-game')
-    title.innerHTML = loseMessage
-    title.innerHTML +='<span class="emoji">&#128580;</span>'
-  } 
-  if(computerChoice == 'scissors' && userChoice=='rock'){
-    panel.style.setProperty('border-color','lightgreen')
-    title.classList.add('win-game')
-    title.innerHTML = winMessage
-    title.innerHTML +='<span class="emoji">&#128522;</span>'
-  } 
-}
-
-function playAgain(){
-  panel.style.setProperty('border-color','gray')
-  iscardchosen = false;
-  possibleChoices.forEach(possibleChoice => {
-    possibleChoice.classList.remove('card_chosen')
-    possibleChoice.classList.remove('card_block')
-  })
-  title.classList.remove('win-game')
-  title.classList.remove('lose-game')
-  title.textContent = 'Choose a card '
-  title.innerHTML += '<span class="emoji">&#129488;</span>'
-  computerChoiceDisplay.style.visibility = "hidden"
+  else{
+    if((computerChoice === 'rock' && userChoice ==='paper') ||
+      (computerChoice === 'paper' && userChoice ==='scissors') ||
+      (computerChoice === 'scissors' && userChoice ==='rock')
+      ){
+        setTitle('win')
+      } 
+    else {
+      if((computerChoice === 'rock' && userChoice ==='scissors') ||
+        (computerChoice === 'paper' && userChoice ==='rock') ||
+        (computerChoice === 'scissors' && userChoice ==='paper'))
+        {
+          setTitle('lose')
+        }
+    }
+  }
 }
 
 function changePage(page){
